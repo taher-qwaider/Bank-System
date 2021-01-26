cace6f8dba9dcf40888490c183286eec1d2b4e7b
Taher qwaider <qwydrtahr@gmail.com>
2021-01-26 12:03:23 +0200

    commit

---
+++ database/migrations/2021_01_26_093333_create_financial_operations_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFinancialOperationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('financial_operations', function (Blueprint $table) {
+            $table->id();
+            $table->string('source_type', 45);
+            $table->bigInteger('source_id');
+            $table->string('destination_type', 45);
+            $table->bigInteger('destination_id');
+            $table->float('amount');
+            $table->tinyInteger('verified');
+            $table->string('verification_code', 45);
+            $table->date('data');
+            $table->time('time');
+
+            $table->foreignId('currency_id');
+            $table->foreign('currency_id')->on('currencies')->references('id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('financial_operations');
+    }
+}
--- resources/views/cms/Cities/index.blade.php
+++ resources/views/cms/Cities/index.blade.php
@@ -15,7 +15,7 @@
             <div class="col-12">
               <div class="card">
                 <div class="card-header">
-                  <h3 class="card-title">Users</h3>
+                  <h3 class="card-title">Cities</h3>
 
                   <div class="card-tools">
                     <div class="input-group input-group-sm" style="width: 150px;">
